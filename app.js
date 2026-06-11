423b6e421460d1c7f67fe7201dcaaa8b7c04b3f5
suhaimitoamy <suhaimitoamy@gmail.com>
2026-06-11 21:12:32 +0800

    Redesign Amy UI: Glassmorphism and Neon accents

--- app.js
+++ app.js
@@ -5425,7 +5425,7 @@ function createAssistantMessageNode(message) {
   if (message.role === "user") {
     avatar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>`;
   } else {
-    avatar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 2a2 2 0 0 1 2 2c0 .74-.4 1.39-1 1.73V7h1a7 7 0 0 1 7 7v1a1 1 0 0 1-1 1h-1v1a5 5 0 0 1-5 5H9a5 5 0 0 1-5-5v-1H3a1 1 0 0 1-1-1v-1a7 7 0 0 1 7-7h1V5.73A2 2 0 0 1 12 2z"></path><path d="M9 13v2"></path><path d="M15 13v2"></path></svg>`;
+    avatar.innerHTML = "A";
   }
 
   const bubble = document.createElement("article");
